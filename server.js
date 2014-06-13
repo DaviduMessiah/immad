ed942fbd394961f11827a51748fd1d4327c19eb4
davidu <davidu.messiah@gmail.com>
2014-06-13 15:55:05 +0530

    removed xexpress

--- server.js
+++ server.js
@@ -4,14 +4,3 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
-
- 
-/*  var express = require("express");
-   var app = express();
-     
-    app.get("/", function(req, res) {
-        res.send("Hello Node.js and Express.");
-    });
-     
-    console.log("Web application opened.");
-    app.listen(process.env.PORT); */
