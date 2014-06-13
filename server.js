4dacb5c4cdc90e33f50c98dfd78ed9f78cd0ddca
davidu <davidu.messiah@gmail.com>
2014-06-13 15:52:44 +0530

    server changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-/* var http = require('http')
+var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
- */
+
  
- var express = require("express");
+/*  var express = require("express");
    var app = express();
      
     app.get("/", function(req, res) {
@@ -14,4 +14,4 @@ http.createServer(function(req, res) {
     });
      
     console.log("Web application opened.");
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT); */
